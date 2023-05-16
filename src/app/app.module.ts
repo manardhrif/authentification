d25543f36f2806919fda116ccec9ce0f5912b30a
Manar <manardhrif@gmail.com>
2023-05-16 15:15:07 +0200

    New project to push to GitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+import { UserService } from './service/user.service';
+import { AuthenticationService } from './service/authentication.service';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { AuthenticationGuard } from './guard/authentication.guard';
+import { NotificationService } from './service/notification.service';
+import { NotificationModule } from './notification.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NotificationModule
+
   ],
-  providers: [],
+  providers: [AuthenticationService , UserService , NotificationService,AuthenticationGuard,
+    { provide :HTTP_INTERCEPTORS , useClass : AuthInterceptor , multi :true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
